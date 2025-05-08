c2979a785d5de9d8251220eec1671b33e01a2755
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-08 22:28:29 +0545

    global toast component for success error message

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,36 +1,3 @@
-
-// import React, { useEffect } from "react";
-// import { useAppDispatch, useAppSelector } from "../redux/hooks";
-// import { hideToast } from "../redux/features/toastSlice";
-
-// const Toast: React.FC = () => {
-//   const { message, type, visible } = useAppSelector((state) => state.toast);
-//   const dispatch = useAppDispatch();
-
-//   useEffect(() => {
-//     if (visible) {
-//       const timer = setTimeout(() => {
-//         dispatch(hideToast());
-//       }, 3000);
-//       return () => clearTimeout(timer);
-//     }
-//   }, [visible, dispatch]);
-
-//   if (!visible) return null;
-
-//   return (
-//     <div
-//       className={`fixed top-5 right-5 px-4 py-2 rounded shadow-md text-white bg-${
-//         type === "success" ? "green" : type === "error" ? "red" : "blue"
-//       }-500 z-50`}
-//     >
-//       {message}
-//     </div>
-//   );
-// };
-
-// export default Toast;
-
 import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { hideToast } from "../redux/features/toastSlice";
