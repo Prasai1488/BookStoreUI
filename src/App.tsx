c9e78f2ad7001aa85e169d9f1984e8c03ea4ce1b
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-08 22:30:17 +0545

    remaining things

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import { Outlet } from "react-router-dom";
-import Toast from "./components/Toast";
+// import { Outlet } from "react-router-dom";
+// import Toast from "./components/Toast";
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <>
+//       <Toast />
+//       <Outlet />
+//     </>
+//   );
+// };
+
+// export default App;
+
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { AuthProvider } from "./context/AuthContext";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loading";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // Cleanup timer
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <Toast />
-      <Outlet />
+      <AuthProvider>
+        <Navbar />
+        <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+          <Outlet />
+        </main>
+        <Footer />
+      </AuthProvider>
     </>
   );
-};
+}
 
 export default App;
--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import Home from "../pages/home/Home";
+import SingleBook from "../pages/books/SingleBook";
 
 const router = createBrowserRouter([
   {
@@ -9,12 +11,20 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
+        path: "/",
+        element: <Home />,
+      },
+      {
         path: "/login",
         element: <Login />,
       },
       {
         path: "/register",
         element: <Register />,
+      },
+      {
+        path: "/books/:id",
+        element: <SingleBook />,
       },
     ],
   },
