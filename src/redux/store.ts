041dd17566efba72cf64e637db8f6103b8e82ffc
Prasai1488 <prasaiprithvi4@gmail.com>
2025-05-08 22:29:50 +0545

    global redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,29 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import toastReducer from "./features/toastSlice";
+// // import other reducers...
+
+// export const store = configureStore({
+//   reducer: {
+//     toast: toastReducer,
+//     // other reducers...
+//   },
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
+
+// store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import toastReducer from "./features/toastSlice";
-// import other reducers...
+import { apiSlice } from "../redux/api/apiSlice"; // <-- import the apiSlice
 
 export const store = configureStore({
   reducer: {
     toast: toastReducer,
-    // other reducers...
+    [apiSlice.reducerPath]: apiSlice.reducer, // <-- add the API reducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware), // <-- add the API middleware
 });
 
 export type RootState = ReturnType<typeof store.getState>;
